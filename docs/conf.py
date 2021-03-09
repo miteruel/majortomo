d9ae6eab98539ac7b4cd88084124c899aaba1d63
Shahar Evron <shahar@shoppimon.com>
2021-03-09 11:26:20 +0200

    Docs related fixes and improvements
    Split docs requirements to separate file
    
    Change docs theme

--- docs/conf.py
+++ docs/conf.py
@@ -11,16 +11,11 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
 
 # -- Project information -----------------------------------------------------
 
 project = 'majortomo'
-copyright = '2018, Shoppimon LTD'
+copyright = '2021, Shoppimon LTD'
 author = 'Shahar Evron'
 
 # The short X.Y version
@@ -80,7 +75,7 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'press'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
